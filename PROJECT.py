ab6e24487e308bd3d0aa0d7bead0fa175e3e2acc
mayurraj876 <62323298+mayurraj876@users.noreply.github.com>
2020-12-05 20:33:31 +0530

    Added code for standardization of dataset with help of z score

--- PROJECT.py
+++ PROJECT.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[82]:
+# In[236]:
 
 
 import numpy as np   
-np.random.seed(42)   ## so that output would be same
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -15,6 +14,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
 from keras.models import Sequential
 from keras.layers import Dense
 #Evaluation
@@ -25,23 +25,22 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 
 # # data processing 
 
-# In[83]:
+# In[237]:
 
 
 data = pd.read_csv("diabetes.csv")
 print(data.info())
-X = data.drop("Outcome",axis=1)
-y = data["Outcome"]
-for attribute in X.columns:
+attributes = data.drop("Outcome",axis=1).columns
+for attribute in attributes:
     plt.figure()
-    X.boxplot([attribute])
+    data.boxplot([attribute])
 
 
-# In[84]:
+# In[238]:
 
 
 data[["Glucose",  "BloodPressure","SkinThickness","Insulin","BMI"]]=data[["Glucose",  "BloodPressure","SkinThickness","Insulin","BMI"]].replace(0,np.nan)
-for attribute in X.columns:
+for attribute in attributes:
     q1 = data[attribute].quantile(0.25)
     q3 = data[attribute].quantile(0.75)
     iqr = q3 - q1
@@ -50,13 +49,13 @@ for attribute in X.columns:
     data.loc[(data[attribute] < fence_low) | (data[attribute] > fence_high),attribute]=np.nan
 
 
-# In[85]:
+# In[239]:
 
 
 print(data.info())
 
 
-# In[86]:
+# In[240]:
 
 
 
@@ -64,11 +63,11 @@ def median_target(var):
     temp = data[data[var].notnull()]
     temp = temp[[var, 'Outcome']].groupby(['Outcome'])[[var]].median().reset_index()
     return temp
-for attribute in X.columns:
+for attribute in attributes:
     print(median_target(attribute))
 
 
-# In[87]:
+# In[241]:
 
 
 data.loc[(data['Outcome'] == 0 ) & (data['Pregnancies'].isnull()), 'Pregnancies'] = 2.0
@@ -89,25 +88,25 @@ data.loc[(data['Outcome'] == 0 ) & (data['Insulin'].isnull()), 'Insulin'] = 102.
 data.loc[(data['Outcome'] == 1 ) & (data['Insulin'].isnull()), 'Insulin'] = 169.5
 
 
-# In[88]:
+# In[242]:
 
 
 
 print(data.info())
 
 
-# In[89]:
+# In[243]:
 
 
-for attribute in X.columns:
+for attribute in attributes:
     plt.figure()
-    X.boxplot([attribute])
+    data.boxplot([attribute])
 
 
-# In[90]:
+# In[244]:
 
 
-print(data.info())
+
 fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(12, 10))
 fig.tight_layout(pad=3.0)
 ax[0,0].set_title('Glucose')
@@ -128,105 +127,130 @@ ax[3,1].set_title('Diabetes Pedigree Function')
 ax[3,1].hist(data.DiabetesPedigreeFunction[data.Outcome==1]);
 
 
-# In[98]:
+# In[245]:
 
 
 
-for attribute in X.columns:
-    plt.figure()
+for attribute in attributes:
+    plt.figure()    
     sns.violinplot(x="Outcome", y=attribute, data=data, palette="muted", split=True)
 
 
-# In[92]:
+# In[246]:
+
+
+
+def z_score(df):
+    """Function for apply z score standardization
+       Input: dataframe to be standardized
+       output :standardized dataframe 
+    """
+    df_std = df.copy()
+    for column in df_std.columns:
+        print(column)
+        df_std[column] = (df_std[column] - df_std[column].mean()) / df_std[column].std()
+        
+    return df_std
+    
+data_std=z_score(data)
+
+
+# In[247]:
 
 
 ## It shows the correlation(positive,neagative) between different columns(only integer value columns) 
-corr_matrix = data.corr()
+corr_matrix = data_std.corr()
 fig,ax = plt.subplots(figsize=(15,10))
 ax = sns.heatmap(corr_matrix,annot=True,linewidth=0.5,fmt=".2f",cmap="RdYlBu")
 
 
 # ###### Distribution of data set 
 
-# In[102]:
+# In[248]:
 
 
-data.sample(frac=1)
-X = data.drop("Outcome",axis=1)
-print(type(X))
+
+
+
 y = data["Outcome"]
+X=data_std.drop("Outcome",axis=1)
+
 X_train,X_test,y_train,y_test =  train_test_split(X,y,test_size=0.2)
 
 
 # 
 # # Models 
 
-# In[101]:
+# In[249]:
 
 
 
 log_reg = LogisticRegression(random_state=0)
 log_reg.fit(X_train,y_train);
-
 ## Evaluating the model
-log_score=cross_val_score(log_reg, X, y, cv=5)
+log_score=cross_val_score(log_reg, X, y, cv=8)
 print("log_reg =",log_score.mean() )
 
 ## Build an model (KNN)
 knn = KNeighborsClassifier()
 knn.fit(X_train,y_train);
 ## Evaluating the model
-knn_score=cross_val_score(knn, X, y, cv=5)
+knn_score=cross_val_score(knn, X, y, cv=8)
 print("knn",knn_score.mean())
 
 
 ## Build an model (Random forest classifier)
 clfm= RandomForestClassifier()
 clfm.fit(X_train,y_train);
-y_pred=clfm.predict(X_test)
 ## Evaluating the model
-clfm_score=cross_val_score(clfm, X, y, cv=5)
-print("clfm",knn_score.mean())#
+clfm_score=cross_val_score(clfm, X, y, cv=8)
+print("clfm",clfm_score.mean())#
 
 # Build an model (Support Vector Machine)
 svm = SVC()
 svm.fit(X_train,y_train)
 ## Evaluating the model
-svm_score=cross_val_score(knn, X, y, cv=5)
+svm_score=cross_val_score(svm, X, y, cv=8)
 print("svm",svm_score.mean())
-"""
+
+ABC = AdaBoostClassifier(n_estimators=100, random_state=0)
+ABC.fit(X, y)
+ABC_score=cross_val_score(ABC, X, y, cv=8)
+print("ABC",ABC_score.mean())
+
+
+# In[250]:
+
+
+
 ##Build an model(Neural model )
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(4, activation='relu'))
+model.add(Dense(8, input_dim=8, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(X_train, y_train, epochs=200, batch_size=10)
 _, nn_acc = model.evaluate(X_test, y_test)
-"""
 
 
-# In[95]:
-
-"""
-#print('Accuracy of Neural network: %.2f' % (nn_acc*100))
-print('Accuracy of logistic regression : %.2f' % (log_reg*100))
-print('Accuracy of Knn: %.2f' % (knn*100))
-print('Accuracy of Random Forest Classifier : %.2f' % (clf*100))
-print('Accuracy of SVM : %.2f' % (svm*100))
+# In[251]:
 
 
-# In[96]:
+print('Accuracy of Neural network: %.2f' % (nn_acc*100))
+print('Accuracy of logistic regression : %.2f' % (log_score.mean()*100))
+print('Accuracy of Knn: %.2f' % (knn_score.mean()*100))
+print('Accuracy of Random Forest Classifier : %.2f' % (clfm_score.mean()*100))
+print('Accuracy of SVM : %.2f' % (svm_score.mean()*100))
 
-model_compare = pd.DataFrame({"Logistic Regression":log_reg,
-"KNN":knn,
-"Random Forest Classifier":clf,
-"Support Vector Machine":svm,"Neural model":nn_acc},
-index=["accuracy"])
-model_compare.T.plot.bar(figsize=(15,10));
 
-"""
+# In[ ]:
+
+
+
+
+
 # In[ ]:
 
 
