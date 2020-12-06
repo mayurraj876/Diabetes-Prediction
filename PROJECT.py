ad3e05e087ab6c3363f87a270af6225f8c4c33fb
mayurraj876 <62323298+mayurraj876@users.noreply.github.com>
2020-12-06 19:04:26 +0530

    Code clean up Removed unnecessary and repeated code

--- PROJECT.py
+++ PROJECT.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[236]:
+# In[134]:
 
 
 import numpy as np   
@@ -23,23 +23,25 @@ from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 
-# # data processing 
+# # Data processing 
 
-# In[237]:
+# In[135]:
 
 
 data = pd.read_csv("diabetes.csv")
-print(data.info())
 attributes = data.drop("Outcome",axis=1).columns
 for attribute in attributes:
     plt.figure()
-    data.boxplot([attribute])
+    sns.violinplot(x="Outcome", y=attribute, data=data, palette="muted", split=True)
 
 
-# In[238]:
+# In[136]:
 
 
+#replacing missing value with nan value
 data[["Glucose",  "BloodPressure","SkinThickness","Insulin","BMI"]]=data[["Glucose",  "BloodPressure","SkinThickness","Insulin","BMI"]].replace(0,np.nan)
+
+#loop for replacing outlier of all attribute with Nan value 
 for attribute in attributes:
     q1 = data[attribute].quantile(0.25)
     q3 = data[attribute].quantile(0.75)
@@ -49,61 +51,30 @@ for attribute in attributes:
     data.loc[(data[attribute] < fence_low) | (data[attribute] > fence_high),attribute]=np.nan
 
 
-# In[239]:
+# In[137]:
 
 
 print(data.info())
 
 
-# In[240]:
+# In[138]:
 
 
-
-def median_target(var):   
-    temp = data[data[var].notnull()]
-    temp = temp[[var, 'Outcome']].groupby(['Outcome'])[[var]].median().reset_index()
-    return temp
+# calculation of median for each attribute for both possible 
+def median_target(attribute):   
+        temp = data[data[attribute].notnull()]# assigning non null value to temp 
+        temp = temp[[attribute, 'Outcome']].groupby(['Outcome'])[[attribute]].median().reset_index() #calculate mean for a attribute with either 0 or 1 outcome 
+        mean_op_0=temp[attribute][0]
+        mean_op_1=temp[attribute][1]
+        data.loc[(data['Outcome'] == 0 ) & (data[attribute].isnull()), attribute] = mean_op_0 #assigning mean to null values 
+        data.loc[(data['Outcome'] == 1 ) & (data[attribute].isnull()), attribute] = mean_op_1
+#calling meadian_target for each attribute
 for attribute in attributes:
-    print(median_target(attribute))
-
-
-# In[241]:
-
-
-data.loc[(data['Outcome'] == 0 ) & (data['Pregnancies'].isnull()), 'Pregnancies'] = 2.0
-data.loc[(data['Outcome'] == 1 ) & (data['Pregnancies'].isnull()), 'Pregnancies'] = 4.0
-data.loc[(data['Outcome'] == 0 ) & (data['DiabetesPedigreeFunction'].isnull()), 'DiabetesPedigreeFunction'] = 0.325
-data.loc[(data['Outcome'] == 1 ) & (data['DiabetesPedigreeFunction'].isnull()), 'DiabetesPedigreeFunction'] = 0.422
-data.loc[(data['Outcome'] == 0 ) & (data['Age'].isnull()), 'Age'] = 26.0
-data.loc[(data['Outcome'] == 1 ) & (data['Age'].isnull()), 'Age'] = 36.0
-data.loc[(data['Outcome'] == 0 ) & (data['BMI'].isnull()), 'BMI'] = 30.1
-data.loc[(data['Outcome'] == 1 ) & (data['BMI'].isnull()), 'BMI'] = 34.3
-data.loc[(data['Outcome'] == 0 ) & (data['BloodPressure'].isnull()), 'BloodPressure'] = 70
-data.loc[(data['Outcome'] == 1 ) & (data['BloodPressure'].isnull()), 'BloodPressure'] = 74.5
-data.loc[(data['Outcome'] == 0 ) & (data['SkinThickness'].isnull()), 'SkinThickness'] = 27
-data.loc[(data['Outcome'] == 1 ) & (data['SkinThickness'].isnull()), 'SkinThickness'] = 32
-data.loc[(data['Outcome'] == 0 ) & (data['Glucose'].isnull()), 'Glucose'] = 107
-data.loc[(data['Outcome'] == 1 ) & (data['Glucose'].isnull()), 'Glucose'] = 140
-data.loc[(data['Outcome'] == 0 ) & (data['Insulin'].isnull()), 'Insulin'] = 102.5
-data.loc[(data['Outcome'] == 1 ) & (data['Insulin'].isnull()), 'Insulin'] = 169.5
-
-
-# In[242]:
-
-
-
+        median_target(attribute) 
 print(data.info())
 
 
-# In[243]:
-
-
-for attribute in attributes:
-    plt.figure()
-    data.boxplot([attribute])
-
-
-# In[244]:
+# In[139]:
 
 
 
@@ -127,7 +98,7 @@ ax[3,1].set_title('Diabetes Pedigree Function')
 ax[3,1].hist(data.DiabetesPedigreeFunction[data.Outcome==1]);
 
 
-# In[245]:
+# In[140]:
 
 
 
@@ -136,10 +107,10 @@ for attribute in attributes:
     sns.violinplot(x="Outcome", y=attribute, data=data, palette="muted", split=True)
 
 
-# In[246]:
+# In[141]:
 
 
-
+# standardization of dataset
 def z_score(df):
     """Function for apply z score standardization
        Input: dataframe to be standardized
@@ -147,18 +118,15 @@ def z_score(df):
     """
     df_std = df.copy()
     for column in df_std.columns:
-        print(column)
-        df_std[column] = (df_std[column] - df_std[column].mean()) / df_std[column].std()
-        
+        df_std[column] = (df_std[column] - df_std[column].mean()) / df_std[column].std()     
     return df_std
-    
 data_std=z_score(data)
 
 
-# In[247]:
+# In[142]:
 
 
-## It shows the correlation(positive,neagative) between different columns(only integer value columns) 
+# It shows the correlation(positive,neagative) between different columns(only integer value columns) 
 corr_matrix = data_std.corr()
 fig,ax = plt.subplots(figsize=(15,10))
 ax = sns.heatmap(corr_matrix,annot=True,linewidth=0.5,fmt=".2f",cmap="RdYlBu")
@@ -166,60 +134,39 @@ ax = sns.heatmap(corr_matrix,annot=True,linewidth=0.5,fmt=".2f",cmap="RdYlBu")
 
 # ###### Distribution of data set 
 
-# In[248]:
-
-
-
+# In[143]:
 
 
 y = data["Outcome"]
 X=data_std.drop("Outcome",axis=1)
-
 X_train,X_test,y_train,y_test =  train_test_split(X,y,test_size=0.2)
 
 
 # 
 # # Models 
 
-# In[249]:
+# In[144]:
 
 
-
-log_reg = LogisticRegression(random_state=0)
-log_reg.fit(X_train,y_train);
-## Evaluating the model
-log_score=cross_val_score(log_reg, X, y, cv=8)
-print("log_reg =",log_score.mean() )
-
-## Build an model (KNN)
-knn = KNeighborsClassifier()
-knn.fit(X_train,y_train);
-## Evaluating the model
-knn_score=cross_val_score(knn, X, y, cv=8)
-print("knn",knn_score.mean())
-
-
-## Build an model (Random forest classifier)
-clfm= RandomForestClassifier()
-clfm.fit(X_train,y_train);
-## Evaluating the model
-clfm_score=cross_val_score(clfm, X, y, cv=8)
-print("clfm",clfm_score.mean())#
-
-# Build an model (Support Vector Machine)
-svm = SVC()
-svm.fit(X_train,y_train)
-## Evaluating the model
-svm_score=cross_val_score(svm, X, y, cv=8)
-print("svm",svm_score.mean())
-
-ABC = AdaBoostClassifier(n_estimators=100, random_state=0)
-ABC.fit(X, y)
-ABC_score=cross_val_score(ABC, X, y, cv=8)
-print("ABC",ABC_score.mean())
+list_of_algo=[SVC(),AdaBoostClassifier(), 
+              RandomForestClassifier(),
+              LogisticRegression(),KNeighborsClassifier()]
+name_of_algo=["SVM","AdaBoostClassifier", 
+              "RandomForestClassifier",
+              "LogisticRegression","KNeighborsClassifier"]
+i=0
+for algorithm in list_of_algo:
+    model=algorithm
+    model.fit(X_train,y_train)
+    model_score=cross_val_score(model,X,y,cv=15)
+    print("*"*120)
+    print('Accuracy of {} : {} '.format(name_of_algo[i],(model_score.mean()*100)))
+    i+=1
+    
+    
 
 
-# In[250]:
+# In[145]:
 
 
 
@@ -235,7 +182,7 @@ model.fit(X_train, y_train, epochs=200, batch_size=10)
 _, nn_acc = model.evaluate(X_test, y_test)
 
 
-# In[251]:
+# In[146]:
 
 
 print('Accuracy of Neural network: %.2f' % (nn_acc*100))
@@ -243,34 +190,4 @@ print('Accuracy of logistic regression : %.2f' % (log_score.mean()*100))
 print('Accuracy of Knn: %.2f' % (knn_score.mean()*100))
 print('Accuracy of Random Forest Classifier : %.2f' % (clfm_score.mean()*100))
 print('Accuracy of SVM : %.2f' % (svm_score.mean()*100))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
