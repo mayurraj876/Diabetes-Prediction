6cb82b405724b63586629078b6990fc70074e800
mayurraj876 <62323298+mayurraj876@users.noreply.github.com>
2020-12-19 19:52:12 +0530

    VALUE ERROR BUG SLOVED

--- app.py
+++ app.py
@@ -6,39 +6,37 @@ app = Flask(__name__)
 
 model = pickle.load(open("Diabetes.pkl", "rb"))
 
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    text1 = request.form['1']
-    text2 = request.form['2']
-    text3 = request.form['3']
-    text4 = request.form['4']
-    text5 = request.form['5']
-    text6 = request.form['6']
-    text7 = request.form['7']
-    text8 = request.form['8']
+    text1 = float(request.form['1'])
+    text2 = float(request.form['2'])
+    text3 = float(request.form['3'])
+    text4 = float(request.form['4'])
+    text5 = float(request.form['5'])
+    text6 = float(request.form['6'])
+    text7 = float(request.form['7'])
+    text8 = float(request.form['8'])
 
-
-    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])
+    row_df = pd.DataFrame(
+        [pd.Series([text1, text2, text3, text4, text5, text6, text7, text8])])
     print(row_df)
 
+    prediction = model.predict_proba(row_df)
 
-    prediction=model.predict_proba(row_df)
+    print(prediction)
+    output = '{0:.{1}f}'.format(prediction[0][1], 2)
+    output = float(output)*100
 
-
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
-    output = str(float(output)*100)+'%'
-
-
-    if output>str(0.5):
-        return render_template('result.html',pred=f'You have chance of having diabetes.\nProbability of having Diabetes is {output}')
+    if output > 50:
+        return render_template('result.html', pred=f'You have chance of having diabetes.\nProbability of having Diabetes is {output}%')
     else:
-        return render_template('result.html',pred=f'You are safe.\n Probability of having diabetes is {output}')
-
+        return render_template('result.html', pred=f'You are safe.\n Probability of having diabetes is {output}%')
 
 
 if __name__ == '__main__':
