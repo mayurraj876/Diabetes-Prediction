e24ae51d4703a8942481005667569cc581e5f753
mayurraj876 <62323298+mayurraj876@users.noreply.github.com>
2020-12-21 11:55:23 +0530

    basics code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+from flask import Flask,request, url_for, redirect, render_template
 import pickle
-from flask import Flask, request, url_for, redirect, render_template
 import pandas as pd
 
 app = Flask(__name__)
@@ -12,31 +12,29 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict',methods=['POST','GET'])
 def predict():
-    text1 = float(request.form['1'])
-    text2 = float(request.form['2'])
-    text3 = float(request.form['3'])
-    text4 = float(request.form['4'])
-    text5 = float(request.form['5'])
-    text6 = float(request.form['6'])
-    text7 = float(request.form['7'])
-    text8 = float(request.form['8'])
-
-    row_df = pd.DataFrame(
-        [pd.Series([text1, text2, text3, text4, text5, text6, text7, text8])])
+    #int_features=[float(x) for x in request.form.values()]
+    text1 = request.form['1']
+    text2 = request.form['2']
+    text3 = request.form['3']
+    text4 = request.form['4']
+    text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
+ 
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])
     print(row_df)
+    prediction=model.predict_proba(row_df)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
 
-    prediction = model.predict_proba(row_df)
-
-    print(prediction)
-    output = '{0:.{1}f}'.format(prediction[0][1], 2)
-    output = float(output)*100
-
-    if output > 50:
-        return render_template('result.html', pred=f'You have a chance of having diabetes.\n Probability of having Diabetes is {output}%')
+    if output>str(0.5):
+        return render_template('index.html',pred='You might have chance of having diabetes.\nProbability of having Diabetes is {}'.format(output))
     else:
-        return render_template('result.html', pred=f'You are safe.\n Probability of having diabetes is {output}%')
+        return render_template('index.html',pred='You are safe.\n Probability of having diabetes is {}'.format(output))
+
+
 
 
 if __name__ == '__main__':
