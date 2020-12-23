0a5fff68ce17640fe95580c4e7c042f29d806586
mayurraj876 <62323298+mayurraj876@users.noreply.github.com>
2020-12-23 19:48:58 +0530

    code formating
    improved commenting
    code clean up

--- app.py
+++ app.py
@@ -24,17 +24,18 @@ def predict():
     bmi = float(request.form['6'])
     diabetes_pedigree_function = float(request.form['7'])
     age = float(request.form['8'])
-    row_df = pd.DataFrame([pd.Series([pregnancy,glucose,blood_pressure,skin_thickness,insulin,bmi,diabetes_pedigree_function,age])])
-    #print(row_df)
-    
+    row_df = pd.DataFrame([pd.Series([pregnancy, glucose, blood_pressure,
+                                      skin_thickness, insulin, bmi, diabetes_pedigree_function, age])])
+    # print(row_df)
+
     prediction = model.predict_proba(row_df)
     output = '{0:.{1}f}'.format(prediction[0][1], 2)
-    #print(output)
+    # print(output)
 
     if output > str(0.5):
-        return render_template('result.html', pred='You might have chance of having diabetes.\n Probability of having Diabetes is {}'.format(output))
+        return render_template('result.html', pred='You might have chance of having diabetes./n Probability of having Diabetes is {}'.format(output))
     else:
-        return render_template('result.html', pred='You are safe.\n Probability of having diabetes is {}'.format(output))
+        return render_template('result.html', pred='You are safe./n Probability of having diabetes is {}'.format(output))
 
 
 if __name__ == '__main__':
