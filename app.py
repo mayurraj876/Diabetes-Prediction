f97bb9050029749352d621b9524fc1c480d44dd5
Mayur Raj Singh Chouhan <62323298+mayurraj876@users.noreply.github.com>
2021-02-12 11:52:54 +0530

    update

--- app.py
+++ app.py
@@ -16,20 +16,20 @@ def home():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    row_dict = {}
-    for attribute in attributes:
-        temp = float(request.form[attribute])
-        row_dict[attribute] = (
-            temp - data[attribute].mean()) / data[attribute].std()
-
-    # print(row_df)
-    #row_df = pd.DataFrame(row_dict)
-    # = pd.DataFrame(row_dict)
-    row_df = pd.Series(row_dict).to_frame()
+    text1 = request.form['1']
+    text2 = request.form['2']
+    text3 = request.form['3']
+    text4 = request.form['4']
+    text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
+ 
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])
     print(row_df)
-    prediction = model.predict_proba(row_df)
-    output = '{0:.{1}f}'.format(prediction[0][1], 2)
-
+    prediction=model.predict_proba(row_df)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    
     if output > str(0.5):
         return render_template('result.html', pred='You might have chance of having diabetes.\n Probability of having Diabetes is {}'.format(output))
     else:
